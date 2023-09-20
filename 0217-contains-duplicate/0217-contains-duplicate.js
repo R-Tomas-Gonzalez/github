eb95b3ad5fccd9f7efedf29e41696368b6a2e3c3
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2023-09-20 12:40:37 -0500

    Time: 87 ms (46.88%), Space: 54.2 MB (51.75%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (!map[num]) {
+            map[num] = 1;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
