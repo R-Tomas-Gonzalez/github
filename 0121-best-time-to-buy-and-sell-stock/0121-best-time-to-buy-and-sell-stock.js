5aa15a51728237d076c251fdd221114f0375e3af
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2023-09-20 22:10:52 -0500

    Time: 79 ms (29.61%), Space: 51.6 MB (43.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,22 +3,23 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let pointer1 = 0;
-    let pointer2 = 1;
+    let left = 0;
+    let right = 1;
 
     let maxProfit = 0;
 
-    while (pointer2 <= prices.length - 1) {
-        if (prices[pointer2] < prices[pointer1]) {
-            pointer1 = pointer2;
-            pointer2++;
+    while (right <= prices.length - 1) {
+        let price1 = prices[left];
+        let price2 = prices[right];
+
+        if (price2 < price1) {
+            left = right;
+            right++;
         } else {
-            let profit = prices[pointer2] - prices[pointer1];
-            maxProfit = Math.max(maxProfit, profit);
-            pointer2++;
+            maxProfit = Math.max(maxProfit, price2 - price1);
+            right++;
         }
     }
-
     return maxProfit;
 };
 
