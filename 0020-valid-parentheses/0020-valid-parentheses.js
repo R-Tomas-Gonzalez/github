cdea5f4585509d1342a61c85d0adcb3a3a0d8fe5
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2023-09-22 09:09:53 -0500

    Time: 56 ms (56.87%), Space: 41.2 MB (99.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,6 +3,8 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    if (s.length % 2 !== 0) return false;
+
     let map = {
         ')' : '(',
         ']' : '[',
@@ -23,14 +25,21 @@ var isValid = function(s) {
         }
     }
 
+    console.log(stack);
+
     return stack.length ? false : true;
 };
 
-// create a stack to go through this array
-// a map will keep track of the opening and closing brackets
+// if there is an odd number of brackets, return false; Because in order to have valid parenthesis we need to have even number parens
+// we want to create a map, to keep track of our parenthesis
+// we want to keep a stack to keep track of our open parens
 
+// for loop, to iterate through the array
+// variable to keep track of our character
 
+// conditional
+// if the character is an opening paren and not a closing paren, we add it to the stack
+// if the current character is a closing bracket and it's value ( the matching opening bracket )is the last character, pop it off the stack
+// if it is not the matching character, return false;
 
-//if we come across a closing bracket, we need to make sure the opening bracket in the last item in the stack
-//if it is we pop it off
-//if our stacks.length is zero at the end, we have our answer
+// at the very end, return whether or not we've been left with any opening brackets in the stack
