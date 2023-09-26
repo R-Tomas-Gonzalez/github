95eed59f77eeef4494d888bac2d38bc5e1741530
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2023-09-26 09:01:47 -0500

    Time: 114 ms (47.55%), Space: 53.2 MB (66.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,19 +3,32 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    if (!strs || strs.length === 1) return [strs]; 
+    if (!strs || strs.length === 1) return [strs];
 
     let map = {};
 
     for (let i = 0; i < strs.length; i++) {
         let sortedWord = strs[i].split('').sort().join('');
+
         if (!map[sortedWord]) {
-            map[sortedWord] = [strs[i]]
+            map[sortedWord] = [strs[i]];
         } else {
             map[sortedWord].push(strs[i]);
         }
     }
 
-    return Object.values(map);;
+    return Object.values(map);
+
 };
 
+
+
+//edge cases
+//we can see in example 2 and example 3 that if the strs are null or length of 1, we can return it
+
+//help from leetcode 242 valid anagram solution
+//strs[i].split('').sort().join('');
+
+//create a map
+//loop through the array
+//return object.values;
