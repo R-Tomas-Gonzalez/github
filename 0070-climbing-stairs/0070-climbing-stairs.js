a348b0d5462bfb03d19eca134599ee05c0d2efe7
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2023-09-29 16:05:48 -0500

    Time: 45 ms (84.13%), Space: 41.9 MB (25.7%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,48 +3,47 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-
     if (n <= 3) return n;
 
-    let dpArr = [1, 1];
+    let dpArray = [2, 3];
 
-    for (let i = 2; i <= n; i++){
-        let sum = dpArr[0] + dpArr[1];
-        dpArr[0] = dpArr[1];
-        dpArr[1] = sum;
+    for (let i = 4; i <= n; i++) {
+        let sum = dpArray[0] + dpArray[1];
+        dpArray[0] = dpArray[1];
+        dpArray[1] = sum
     }
 
-    return dpArr[1];
-
+    return dpArray[1];
 };
 
-//subproblems
-//we can either climb 1 or 2 stairs
-//essentially this comes down to a dp problem.
-//because it's a fib problem
+// subproblems
+// we can either climb 1 or 2 stairs
+// essentially this comes down to a dp problem.
+// because it's a fib problem
+// [1, 1, 2, 3, 5, 8]
 
-//2 stairs = 2
-//1 + 1
-//2
+// 2 stairs = 2
+// 1 + 1
+// 2
 
-//3 stairs = 3
-//1 + 1 + 1
-//2 + 1
-//1 + 2
+// 3 stairs = 3
+// 1 + 1 + 1
+// 2 + 1
+// 1 + 2
 
-//4 stairs = 4
-//2 + 2
-//2 + 1 + 1
-//1 + 1 + 2
-//1 + 2 + 1
-//1 + 1 + 1 + 1
+// 4 stairs = 5
+// 2 + 2
+// 2 + 1 + 1
+// 1 + 1 + 2
+// 1 + 2 + 1
+// 1 + 1 + 1 + 1
 
-//5 stairs = 8
-//1 + 1 + 1 + 2
-//1 + 1 + 2 + 1
-//1 + 2 + 1 + 1
-//2 + 1 + 1 + 1
-//1 + 1 + 1 + 1 + 1
-//2 + 2 + 1
-//1 + 2 + 2
-//2 + 1 + 2
+// 5 stairs = 8
+// 1 + 1 + 1 + 2
+// 1 + 1 + 2 + 1
+// 1 + 2 + 1 + 1
+// 2 + 1 + 1 + 1
+// 1 + 1 + 1 + 1 + 1
+// 2 + 2 + 1
+// 1 + 2 + 2
+// 2 + 1 + 2
