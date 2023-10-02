29a3ed2bdccdc1e1ce2ddfc301c6b061a45b2df5
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2023-10-02 17:03:28 -0500

    Time: 66 ms (75.14%), Space: 42.7 MB (84.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,13 +4,27 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let newS = s.split('').sort().join('');
-    let newT = t.split('').sort().join('');
+    if (s.length !== t.length) return false
 
-    return newS === newT ? true : false;
-    if (newS === newT) {
-        return true;
-    } else {
-        return false;
+    let map = {};
+
+    for (let i = 0; i < s.length; i++) {
+        let letter = s[i];
+        if (!map[letter]) {
+            map[letter] = 1;
+        } else {
+            map[letter]++;
+        }
     }
+
+    for (let j = 0; j < t.length; j++) {
+        let letter = t[j];
+        if (map[letter] && map[letter] !== 0) {
+            map[letter]--;
+        } else {
+            return false;
+        }
+    }   
+
+    return true;
 };
