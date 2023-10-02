a577dc95bcd3d657c5098c00f05de7e3a6b965c7
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2023-10-02 10:36:52 -0500

    Time: 42 ms (93.35%), Space: 41.7 MB (47.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -5,23 +5,23 @@
 var climbStairs = function(n) {
     if (n <= 3) return n;
 
-    let dpArr = [ 2, 3 ];
-
+    let var1 = 2;
+    let var2 = 3;
+    
     for (let i = 4; i <= n; i++) {
-        let sum = dpArr[0] + dpArr[1];
-        dpArr[0] = dpArr[1];
-        dpArr[1] = sum;
+        let temp = var1 + var2;
+        var1 = var2;
+        var2 = temp;
     }
 
-    return dpArr[1];
+    return var2
 };
 
 // subproblems
 // we can either climb 1 or 2 stairs
 // essentially this comes down to a dp problem.
 // because it's a fib problem
-// [1, 1, 2, 3, 5, 8, 13]
-
+// [ 1, 2, 3, 5, 8, 13]
 
 // 2 stairs = 2
 // 1 + 1
