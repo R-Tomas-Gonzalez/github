b4abefd3fccd56bfd7148ddcce7c02ca52bb7a00
Tomas Gonzalez <63771558+R-Tomas-Gonzalez@users.noreply.github.com>
2023-10-09 11:16:59 -0500

    Time: 79 ms (54.21%), Space: 49.7 MB (14.8%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -6,26 +6,41 @@ var lengthOfLongestSubstring = function(s) {
     let pointer1 = 0;
     let pointer2 = 0;
 
-    let maxSubstring = 0;
+    let map = {};
+    let count = 0;
+    
+    while (pointer2 < s.length) {
+        let letter1 = s[pointer1];
+        let letter2 = s[pointer2];
 
-    let map = {}
-
-    while(pointer2 < s.length) {
-        if (!map[s[pointer2]]) {
-            map[s[pointer2]] = s[pointer2];
+        if (!map[letter2]) {
+            map[letter2] = letter2;
             pointer2++;
-            maxSubstring = Math.max(maxSubstring, (pointer2 - pointer1));
+            count = Math.max(count, (pointer2 - pointer1));
         } else {
-            delete map[s[pointer1]];
+            delete map[letter1];
             pointer1++;
         }
     }
 
-    return maxSubstring;
+
+    return count;
 };
 
-// implement a sliding window technique
-// we need to keep track of our letters and duplicates
-// when we keep track of duplicates, we implement a hash set
-// we keep track if there are any dupes in the hash
-// and if there are, we increment first pointer
+/*
+//example
+let map = {
+    'c' = 'c',
+    'a' = 'a',
+    'b' = 'b'
+
+};
+
+//create a map
+//checks if letter is in map
+//initializes key and value to letter
+//increment pointer2 to keep going
+//check the max vs the distance
+//get rid of pointer1 in map
+//increment pointer1 until we get to a new non duplicate index
+*/
